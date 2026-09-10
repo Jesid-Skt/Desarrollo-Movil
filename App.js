e3bb8fa828838ab3430820d383b71fd41c7da62c
jesidsantiago <jesidsantiago90@gmail.com>
2026-09-10 13:15:11 -0500

    crecion de archivos para agregar los componentes de reservas

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import ClasesStack from './src/navigation/ClasesStack';
+import  ClasesStack  from './src/navigation/ClasesStacks';
 import { colors } from './src/theme';
 
 const temaNavegacion = {
